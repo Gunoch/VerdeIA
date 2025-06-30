8a3174c8e3a9416cdcaf0f5f4f8a5fc899934315
Gu No <gustavo.nolasco1000@gmail.com>
2025-06-30 02:17:08 +0000

    vc tirou a imagem que stava la

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
 import { Leaf, Gauge, Search, Brain } from "lucide-react"; // Alterado ícone de Replace para Search/Brain
